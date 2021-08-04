f4ca4b8cea761e8c2d5ec64546d2e7c4e9651be9
Hovhannes Kocharyan <hovkocharyan@yandex.com>
2021-08-04 14:30:40 +0400

    Refactor: Separate Header

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import firebase from 'firebase/app'
 import firebaseConfig from './constants/firebase.config'
 // Components
 import Home from './pages/Home/Home'
+import Header from './components/sections/Header/Header'
 
 firebase.initializeApp(firebaseConfig)
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Home />
     </div>
   )
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 // Components
-import Header from '../../components/sections/Header/Header'
 import MoviesList from '../../components/sections/MoviesList/MoviesList'
 import TitleSection from '../../components/sections/TitleSection/TitleSection'
 
 export default function Home() {
   return (
     <div>
-      <Header />
       <TitleSection text={'Movies'} />
       <MoviesList />
     </div>
